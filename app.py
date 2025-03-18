7def156d3a96c79e5662082b25d581b2673e7fdc
emcappusr1 <phanikirankolli4209@gmail.com>
2025-03-18 13:12:24 +0530

    otp removed

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from apis.profile import profile_bp
 from apis.dashboard import dashboard_bp
 from apis.assessment import assessment_bp
 from apis.admin import admin_bp
-from apis.otp import otp_bp
 from apis.prediction import prediction_bp  # Import the new prediction blueprint
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -22,7 +21,6 @@ app.register_blueprint(profile_bp, url_prefix='/profile')
 app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 app.register_blueprint(assessment_bp, url_prefix='/assessment')
 app.register_blueprint(admin_bp, url_prefix='/admin')
-app.register_blueprint(otp_bp, url_prefix='/otp')
 app.register_blueprint(prediction_bp, url_prefix='/prediction')  # Register the new prediction blueprint
 
 if __name__ == '__main__':
